da0d201bd2e48f675570b2e8a0cbdcf0b0aef11b
vicfirth@1997 <yashmundra1997@gmail.com>
2025-03-05 12:54:52 +0530

    node intro

---
+++ myMathModule.js
@@ -0,0 +1,6 @@
+
+function add10(n) {
+    return n+10;
+}
+
+module.exports = add10;
---
+++ script.js
@@ -0,0 +1,12 @@
+function add(a,b) {
+    return a+b
+}
+
+const ans=add(10,30);
+console.log(ans)
+
+
+const add10 = require("./myMathModule");
+
+const res=add10(10);
+console.log(res)
